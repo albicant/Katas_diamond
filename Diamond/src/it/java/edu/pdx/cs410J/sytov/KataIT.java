6be038e7fff5f23aaf94c21124189766b6cdf3aa
albicant <silvics@protonmail.com>
2020-01-29 19:37:56 -0800

    Added command line argument tests

--- Diamond/src/it/java/edu/pdx/cs410J/sytov/KataIT.java
+++ Diamond/src/it/java/edu/pdx/cs410J/sytov/KataIT.java
@@ -9,20 +9,44 @@ import static org.hamcrest.core.StringContains.containsString;
 
 public class KataIT extends InvokeMainTestCase {
 
+  private MainMethodResult invokeMain(String... args) {
+    return invokeMain( Kata.class, args );
+  }
+
   @Test
   public void invokingMainWithNoArgumentsHasExitCodeOf1() {
-    InvokeMainTestCase.MainMethodResult result = invokeMain(Kata.class);
+    MainMethodResult result = invokeMain();
     assertThat(result.getExitCode(), equalTo(1));
   }
 
   @Test
   public void invokingMainWithNoArgumentsPrintsMissingArgumentsToStandardError() {
-    InvokeMainTestCase.MainMethodResult result = invokeMain(Kata.class);
+    MainMethodResult result = invokeMain();
     assertThat(result.getTextWrittenToStandardError(), containsString("Missing command line arguments"));
   }
 
-//  @Test
-//  public void testKataExceptsCharAsInput
+  @Test
+  public void testKataAcceptsCharAsInput() {
+    String[] args = new String[]{"A"};
+    MainMethodResult result = invokeMain(args);
+    assertThat(result.getExitCode(), equalTo(0));
 
+  }
+
+  @Test
+  public void testKataRejectsMultipleChars() {
+    String[] args = new String[]{"A", "B"};
+    MainMethodResult result = invokeMain(args);
+    assertThat(result.getExitCode(), equalTo(1));
+    assertThat(result.getTextWrittenToStandardError(), containsString("To many command line arguments"));
+  }
+
+  @Test
+  public void testKataRejectsLongString() {
+    String[] args = new String[]{"Abacadabra"};
+    MainMethodResult result = invokeMain(args);
+    assertThat(result.getExitCode(), equalTo(1));
+    assertThat(result.getTextWrittenToStandardError(), containsString("The String has to contain only 1 character"));
+  }
 
 }
--- Diamond/src/main/java/edu/pdx/cs410J/sytov/Kata.java
+++ Diamond/src/main/java/edu/pdx/cs410J/sytov/Kata.java
@@ -10,7 +10,21 @@ public class Kata {
                                                                                     
 
   public static void main(String[] args) {
-    System.err.println("Missing command line arguments");
-    System.exit(1);
+    if (args.length < 1) {
+      System.err.println("Missing command line arguments");
+      System.exit(1);
+    }
+    else if (args.length > 1) {
+      System.err.println("To many command line arguments");
+      System.exit(1);
+    }
+
+    if(args[0].length() != 1) {
+      System.err.println("The String has to contain only 1 character");
+      System.exit(1);
+    }
+
+    char c = args[0].charAt(0);
+    System.exit(0);
   }
 }
