742d2a0ee53d597403d8c314112dd2219cda1e86
albicant <silvics@protonmail.com>
2020-01-29 19:19:05 -0800

    Created the Diamond project

---
+++ Diamond/src/it/java/edu/pdx/cs410J/sytov/KataIT.java
@@ -0,0 +1,28 @@
+package edu.pdx.cs410J.sytov;
+
+import edu.pdx.cs410J.InvokeMainTestCase;
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.hamcrest.core.StringContains.containsString;
+
+public class KataIT extends InvokeMainTestCase {
+
+  @Test
+  public void invokingMainWithNoArgumentsHasExitCodeOf1() {
+    InvokeMainTestCase.MainMethodResult result = invokeMain(Kata.class);
+    assertThat(result.getExitCode(), equalTo(1));
+  }
+
+  @Test
+  public void invokingMainWithNoArgumentsPrintsMissingArgumentsToStandardError() {
+    InvokeMainTestCase.MainMethodResult result = invokeMain(Kata.class);
+    assertThat(result.getTextWrittenToStandardError(), containsString("Missing command line arguments"));
+  }
+
+//  @Test
+//  public void testKataExceptsCharAsInput
+
+
+}
